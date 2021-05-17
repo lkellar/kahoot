f97ccde2fe041df2732b1bc08cfed703e11cef80
Lucas K <lucas@lkellar.org>
2021-05-17 18:53:12 -0500

    added numbers

--- options/options.js
+++ options/options.js
@@ -69,7 +69,7 @@ async function restoreOptions() {
     }
 }
 
-const acceptableMappings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '`', '-', '=', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f20'];
+const acceptableMappings = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '`', '-', '=', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f20', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 const defaults = {
     triangle: 'o',
